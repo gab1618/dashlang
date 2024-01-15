52c2016d2332d5acca0e80979fe9e9bcb6a01c10
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-01-15 15:42:08 -0300

    feat: add tokens

---
+++ packages/tokens/src/lib.rs
@@ -0,0 +1,41 @@
+pub struct AsignmentExpr {
+    symbol: String,
+    value: Box<Expr>,
+}
+pub enum BinaryOpType {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Gt, // Greater than
+    Eq, // Equal
+    Ge, // Greater or equal than
+    Lt, // Less than
+    Le, // Less of equal than
+}
+pub struct BinaryOp {
+    left: Expr,
+    right: Expr,
+    op_type: BinaryOpType,
+}
+pub struct Call {
+    symbol: String,
+    args: Vec<Expr>,
+}
+pub struct Closure {
+    symbol: String,
+    params: Vec<Expr>,
+    body: Vec<Expr>,
+}
+pub enum Expr {
+    BinaryOp(Box<BinaryOp>),
+    AsignmentExpr(AsignmentExpr),
+    Closure(Closure),
+    Call(Call),
+    Int(i64),
+    Float(f64),
+    String(String),
+    Bool(bool),
+    Symbol(String),
+    Void,
+}
