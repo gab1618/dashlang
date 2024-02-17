3988210ce191f8c5ebcb948fe78976fae71e4b60
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-02-17 20:04:25 -0300

    feat: Implement program parsing

--- packages/parse/src/lib.rs
+++ packages/parse/src/lib.rs
@@ -5,9 +5,12 @@ use values::parse_values;
 mod expression;
 mod instruction;
 mod parser;
+mod program;
 mod statement;
 mod values;
 
+use program::parse_program;
+
 pub fn parse(input: &str) -> Program {
-    todo!()
+    parse_program(input)
 }
---
+++ packages/parse/src/program.rs
@@ -0,0 +1,42 @@
+use ast::Program;
+use pest::Parser;
+
+use crate::{
+    instruction::parse_instruction,
+    parser::{DashlangParser, Rule},
+};
+
+pub fn parse_program(input: &str) -> Program {
+    let mut program: Program = vec![];
+    let ast = DashlangParser::parse(Rule::program, input)
+        .expect("Could not parse program")
+        .next()
+        .expect("Could not parse program");
+    for instruction in ast.into_inner() {
+        program.push(parse_instruction(instruction.as_str()));
+    }
+    program
+}
+
+#[cfg(test)]
+mod tests {
+    use ast::{Asignment, Expr, Instruction, Value};
+
+    use super::*;
+    #[test]
+    fn test_parse_program() {
+        assert_eq!(
+            parse_program("age = 5 count = 1"),
+            vec![
+                Instruction::Expr(Expr::Asignment(Asignment {
+                    symbol: String::from("age"),
+                    value: Box::new(Expr::Value(Value::Int(5)))
+                })),
+                Instruction::Expr(Expr::Asignment(Asignment {
+                    symbol: String::from("count"),
+                    value: Box::new(Expr::Value(Value::Int(1)))
+                }))
+            ]
+        )
+    }
+}
