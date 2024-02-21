8e26fa3bfb732c5baed5a50a564e012548ed2eb2
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-02-21 18:16:20 -0300

    feat: add if and while statements to parse statement function

--- packages/parse/src/statement/mod.rs
+++ packages/parse/src/statement/mod.rs
@@ -8,6 +8,8 @@ use crate::parser::{DashlangParser, Rule};
 
 use return_stmt::parse_return_stmt;
 
+use self::{if_stmt::parse_if_stmt, while_stmt::parse_while_stmt};
+
 pub fn parse_statement(input: &str) -> Stmt {
     let ast = DashlangParser::parse(Rule::statement, input)
         .expect("Could not parse statement")
@@ -16,12 +18,14 @@ pub fn parse_statement(input: &str) -> Stmt {
     let ast_statement = ast.into_inner().next().expect("Could not get statement");
     match ast_statement.as_rule() {
         Rule::return_stmt => parse_return_stmt(ast_statement.as_str()),
+        Rule::if_stmt => Stmt::If(parse_if_stmt(ast_statement.as_str())),
+        Rule::while_stmt => Stmt::While(parse_while_stmt(ast_statement.as_str())),
         _ => unreachable!(),
     }
 }
 #[cfg(test)]
 mod tests {
-    use ast::{Expr, Stmt, Value};
+    use ast::{BinaryOp, BinaryOpType, Expr, If, Stmt, Value, While};
 
     use super::*;
     #[test]
@@ -29,6 +33,35 @@ mod tests {
         assert_eq!(
             parse_statement("return 5"),
             Stmt::Return(Expr::Value(Value::Int(5)))
+        );
+    }
+    #[test]
+    fn test_parse_if() {
+        assert_eq!(
+            parse_statement("if count < 5 {}"),
+            Stmt::If(If {
+                cond: Expr::BinaryOp(Box::new(BinaryOp {
+                    left: Expr::Symbol(String::from("count")),
+                    right: Expr::Value(Value::Int(5)),
+                    op_type: BinaryOpType::Lt
+                })),
+                else_block: None,
+                body: vec![]
+            })
+        );
+    }
+    #[test]
+    fn test_parse_while() {
+        assert_eq!(
+            parse_statement("while count < 5 {}"),
+            Stmt::While(While {
+                cond: Expr::BinaryOp(Box::new(BinaryOp {
+                    left: Expr::Symbol(String::from("count")),
+                    right: Expr::Value(Value::Int(5)),
+                    op_type: BinaryOpType::Lt
+                })),
+                body: vec![]
+            })
         );
     }
 }
