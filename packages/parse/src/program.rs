61dbc747a9dd89f1a9d13bad7cb9e3ababd6d6a4
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-02-24 16:22:03 -0300

    test: add tests using entire files

--- packages/parse/src/program.rs
+++ packages/parse/src/program.rs
@@ -20,7 +20,9 @@ pub fn parse_program(input: &str) -> Program {
 
 #[cfg(test)]
 mod tests {
-    use ast::{Asignment, Expr, Instruction, Value};
+    use std::path::Path;
+
+    use ast::{Asignment, BinaryOp, BinaryOpType, Expr, Instruction, Stmt, Value, While};
 
     use super::*;
     #[test]
@@ -38,5 +40,50 @@ mod tests {
                 }))
             ]
         )
+    }
+    #[test]
+    fn test_from_file() {
+        let examples_dir_path = std::env::current_dir()
+            .expect("Could not get current dir")
+            .join(Path::new("examples"));
+        let hello_world_path = examples_dir_path.join(Path::new("hello_world.dash"));
+        let hello_world_content = std::fs::read_to_string(hello_world_path).unwrap();
+        let parsed = parse_program(&hello_world_content);
+        assert_eq!(
+            parsed,
+            vec![Instruction::Stmt(Stmt::Print(Expr::Value(Value::String(
+                String::from("Hello, World!")
+            ))))]
+        );
+        let while_path = examples_dir_path.join(Path::new("while.dash"));
+        let while_content = std::fs::read_to_string(while_path).unwrap();
+        let parsed = parse_program(&while_content);
+        assert_eq!(
+            parsed,
+            vec![
+                Instruction::Expr(Expr::Asignment(Asignment {
+                    symbol: String::from("count"),
+                    value: Box::new(Expr::Value(Value::Int(0)))
+                })),
+                Instruction::Stmt(Stmt::While(While {
+                    cond: Expr::BinaryOp(Box::new(BinaryOp {
+                        left: Expr::Symbol(String::from("count")),
+                        right: Expr::Value(Value::Int(10)),
+                        op_type: BinaryOpType::Lt
+                    })),
+                    body: vec![
+                        Instruction::Stmt(Stmt::Print(Expr::Symbol(String::from("count")))),
+                        Instruction::Expr(Expr::Asignment(Asignment {
+                            symbol: String::from("count"),
+                            value: Box::new(Expr::BinaryOp(Box::new(BinaryOp {
+                                left: Expr::Symbol(String::from("count")),
+                                right: Expr::Value(Value::Int(1)),
+                                op_type: BinaryOpType::Add
+                            })))
+                        }))
+                    ]
+                }))
+            ]
+        );
     }
 }
