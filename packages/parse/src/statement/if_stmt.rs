8c39ed482207374d9a7442b58c3eaca422ff90e4
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-03-29 18:10:35 -0300

    perf(parse): optimize if statement parsing

--- packages/parse/src/statement/if_stmt.rs
+++ packages/parse/src/statement/if_stmt.rs
@@ -1,45 +1,29 @@
-use ast::{Expr, If, Literal};
+use ast::If;
 use pest::Parser;
 
 use crate::{
     expression::parse_expression,
-    literal::parse_literal,
     parser::{DashlangParser, Rule},
     scope::parse_scope,
 };
 
 pub fn parse_if_stmt(input: &str) -> If {
-    let mut final_if = If {
-        cond: Expr::Literal(Literal::Bool(true)),
-        body: vec![],
-        else_block: None,
-    };
     let ast = DashlangParser::parse(Rule::if_stmt, input)
         .expect("Could not parse if statement")
         .next()
         .expect("Could not parse if statement");
-    for element in ast.into_inner() {
-        match element.as_rule() {
-            Rule::expression => {
-                final_if.cond = parse_expression(element.as_str());
-            }
-            Rule::literal => {
-                final_if.cond = Expr::Literal(parse_literal(element.as_str()));
-            }
-            Rule::scope => {
-                final_if.body = parse_scope(element.as_str());
-            }
-            Rule::else_stmt => {
-                let else_block_ast = element
-                    .into_inner()
-                    .next()
-                    .expect("Could not get scope from else block");
-                final_if.else_block = Some(parse_scope(else_block_ast.as_str()));
-            }
-            _ => unreachable!(),
-        }
+    let mut inner_ast = ast.into_inner();
+    let ast_cond = parse_expression(inner_ast.next().expect("Could not get condition").as_str());
+    let ast_body = parse_scope(inner_ast.next().expect("Could not get scope").as_str());
+    let ast_else = match inner_ast.next() {
+        Some(pair) => Some(parse_scope(pair.into_inner().next().unwrap().as_str())),
+        None => None,
+    };
+    If {
+        cond: ast_cond,
+        body: ast_body,
+        else_block: ast_else,
     }
-    final_if
 }
 #[cfg(test)]
 mod tests {
